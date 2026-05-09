7fecf35b19b80a4d732d53c906c89efae0aada7e
Daniel Rudolf <github.com@daniel-rudolf.de>
2026-05-09 15:28:50 +0200

    Update .bashrc
    Make sure that `~/.bashrc` and `~/bashrc.d/bash_aliases.sh` only sources `*.sh` files, not arbitrary files.

--- dotfiles/dot_bashrc.d/bash_aliases.sh
+++ dotfiles/dot_bashrc.d/bash_aliases.sh
@@ -1,6 +1,6 @@
 # include drop-in files in ~/.bashrc.d/bash_aliases.d directory
 if [ -d "$(dirname "${BASH_SOURCE[0]}")/bash_aliases.d" ]; then
-    for __bashalias_file in "$(dirname "${BASH_SOURCE[0]}")/bash_aliases.d/"*; do
+    for __bashalias_file in "$(dirname "${BASH_SOURCE[0]}")/bash_aliases.d/"*".sh"; do
         [ -f "$__bashalias_file" ] || continue
         . "$__bashalias_file"
     done

ec268911ff79ae7f099b800ca595b84d4cfe6819
Daniel Rudolf <github.com@daniel-rudolf.de>
2026-05-10 20:42:39 +0200

    Update .bashrc.d/less.sh
    Always export the `LESS` env variable and use a better default value.

--- dotfiles/dot_bashrc.d/less.sh
+++ dotfiles/dot_bashrc.d/less.sh
@@ -1,8 +1,13 @@
-# enable lesspipe for `less`, internally using bat, GNU Source-highlight, etc.,
-# or directly fall back to GNU's Source-highlight if lesspipe isn't availabe
-if [ -x "$(type -p lesspipe.sh)" ]; then
-    export LESSOPEN="| $(type -p lesspipe.sh) %s"
-elif [ -x "$(type -p src-hilite-lesspipe.sh)" ]; then
-    export LESSOPEN="| $(type -p src-hilite-lesspipe.sh) %s"
-    export LESS=' -R '
+if [ -x "$(type -p less)" ]; then
+    # enable lesspipe for `less`, internally using bat, GNU Source-highlight, etc.,
+    # or fall back to plain GNU's Source-highlight if lesspipe isn't availabe
+    if [ -x "$(type -p lesspipe.sh)" ]; then
+        export LESSOPEN="| $(type -p lesspipe.sh) %s"
+    elif [ -x "$(type -p src-hilite-lesspipe.sh)" ]; then
+        export LESSOPEN="| $(type -p src-hilite-lesspipe.sh) %s"
+    fi
+
+    # quit if contents fit into one screen (-F), and enable raw ANSI colors (-R)
+    # different from typical defaults, don't restore the last shown screen (no -X)
+    export LESS='-FR'
 fi

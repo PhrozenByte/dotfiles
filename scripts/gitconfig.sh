76d25699105c04db3e87dbfa136db17b17b8531b
Daniel Rudolf <github.com@daniel-rudolf.de>
2026-09-08 23:57:21 +0200

    Add docs to `gitconfig.sh` script

--- scripts/gitconfig.sh
+++ scripts/gitconfig.sh
@@ -1,6 +1,72 @@
 #!/bin/bash
 # gitconfig.sh
-# Updates ~/.gitconfig.d/* config files from a JSON file.
+# Updates ~/.gitconfig.d/* config files from JSON files.
+#
+# This script reads Git config values from JSON files (passed as arguments)
+# and writes them to the local user's Git config. The config is split into
+# a global user config and optional remote-specific configs.
+#
+# Example JSON file:
+#
+# ```json
+# {
+#     "config": {
+#         "user": { "name": "Daniel Rudolf" }
+#     },
+#     "remotes": [
+#         {
+#             "includeFile": "github.com",
+#             "remoteUrlPattern": [
+#                 "git@github.com:*/**"
+#             ],
+#             "config": {
+#                 "user": { "name": "PhrozenByte" }
+#             }
+#         }
+#     ]
+# }
+# ```
+#
+# The JSON file can contain a `config` object whose scalar values are written
+# to `~/.gitconfig.d/user`. Nested objects are converted to Git's dotted-key
+# notation. In the example above, it will write `user.name = Daniel Rudolf`
+# to `~/.gitconfig.d/user`. Consequently, Git will use "Daniel Rudolf" as the
+# default commit author name.
+#
+# The optional `remotes` key contains a list of remote-specific config
+# objects. Each entry must define an `includeFile` filename and at least one
+# `remoteUrlPattern`. The patterns are used to create conditional Git config
+# includes in `~/.gitconfig.d/remotes`. The corresponding `config` object is
+# written to `~/.gitconfig.d/remotes.d/<includeFile>`, using the same syntax
+# as the global `config` object.
+#
+# Git therefore conditionally loads the appropriate remote-specific config
+# based on the URL of the repository's remote. This can be used, for example,
+# to use different Git identities or signing keys for different hosting
+# providers while keeping the config in a single JSON source file.
+#
+# In the example above, the script will write `user.name = PhrozenByte` to
+# `~/.gitconfig.d/remotes.d/github.com`. This file is conditionally included
+# via `~/.gitconfig.d/remotes` when any of a repository's remote URLs matches
+# `git@github.com:*/**`. Consequently, Git will use "PhrozenByte" as the
+# default commit author name for matching repositories.
+#
+# Overall, the above example results in a global Git identity and a separate
+# identity for repositories whose remote URLs match the configured patterns.
+#
+# To actually use the config files created by this script, add the following
+# includes to your `~/.gitconfig`:
+#
+# ```
+# [include]
+#     path = ~/.gitconfig.d/user
+# [include]
+#     path = ~/.gitconfig.d/remotes
+# ```
+#
+# Multiple JSON files may be passed at once. They are processed in the order
+# given on the command line; later files may therefore override configuration
+# values written by earlier files.
 #
 # Copyright (C) 2026 Daniel Rudolf (<https://www.daniel-rudolf.de>)
 # License: The MIT License <http://opensource.org/licenses/MIT>
